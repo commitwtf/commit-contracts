a19a52bb1b72fcf40c69d38865428d3bacd7bbe0
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-01-11 12:28:22 +0100

    Fix compiler warnings

--- src/mocks/EASIndexerMock.sol
+++ src/mocks/EASIndexerMock.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.20;
 
 contract EASIndexerMock {
     function getSchemaAttesterRecipientAttestationUIDCount(
-        bytes32 schemaUID,
-        address attester,
-        address recipient
-    ) external view returns (uint256) {
+        bytes32,
+        address,
+        address
+    ) external pure returns (uint256) {
         return 1;
     }
 }
