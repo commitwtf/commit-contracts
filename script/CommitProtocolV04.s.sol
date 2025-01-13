36bfb18cb553b2e812114e4d7cba3a60cbe28347
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-01-13 21:49:23 +0100

    Update deploy script

--- script/CommitProtocolV04.s.sol
+++ script/CommitProtocolV04.s.sol
@@ -2,18 +2,38 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 import {CommitProtocolV04} from "../src/CommitProtocolV04.sol";
+import {CommitProtocolERC1155} from "../src/CommitProtocolERC1155.sol";
 
-contract CounterScript is Script {
+contract DeployCommitProtocol is Script {
     CommitProtocolV04 public protocol;
 
     function setUp() public {}
 
     function run() public {
+        address owner = vm.envAddress("PROTOCOL_OWNER_ADDRESS");
+        address protocolFeeRecipient = vm.envAddress("PROTOCOL_FEE_ADDRESS");
         vm.startBroadcast();
 
-        protocol = new CommitProtocolV04();
+        address proxy =
+            Upgrades.deployUUPSProxy("CommitProtocolV04.sol", abi.encodeCall(CommitProtocolERC1155.initialize, (owner)));
 
+        CommitProtocolV04.ProtocolConfig memory config = CommitProtocolV04.ProtocolConfig({
+            maxCommitDuration: 30 days,
+            baseURI: "https://example.com/",
+            fee: CommitProtocolV04.ProtocolFee({
+                recipient: protocolFeeRecipient,
+                fee: 0.01 ether, // protocol creation/join fee in ETH
+                shareBps: 500 // 5%
+            })
+        });
+
+        CommitProtocolV04(proxy).setProtocolConfig(config);
+
+        console.log("CommitProtocol proxy deployed to:", proxy);
+        console.log("Protocol owner address set to:", owner);
+        console.log("Protocol fee address set to:", protocolFeeRecipient);
         vm.stopBroadcast();
     }
 }
