9a4d4c9b8abf69941ae12e0ae24cb9e8d6c6f3cb
revmiller <revmiller@pm.me>
2025-01-16 12:42:45 +0100

    refactor: cleanup deployment script

--- script/CommitProtocolV04.s.sol
+++ script/CommitProtocolV04.s.sol
@@ -7,9 +7,6 @@ import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 import {CommitProtocolV04} from "../src/CommitProtocolV04.sol";
 
 contract DeployCommitProtocol is Script {
-    CommitProtocolV04 public protocol;
-
-    function setUp() public {}
 
     function run() public {
         address owner = vm.envAddress("PROTOCOL_OWNER_ADDRESS");
@@ -21,21 +18,10 @@ contract DeployCommitProtocol is Script {
                 "CommitProtocolV04.sol",
                 abi.encodeCall(CommitProtocolV04.initialize, (owner)));
 
-        CommitProtocolV04.ProtocolConfig memory config = CommitProtocolV04.ProtocolConfig({
-            maxCommitDuration: 30 days,
-            baseURI: "https://example.com/",
-            fee: CommitProtocolV04.ProtocolFee({
-                recipient: protocolFeeRecipient,
-                fee: 0.01 ether, // protocol creation/join fee in ETH
-                shareBps: 500 // 5%
-            })
-        });
-
-        CommitProtocolV04(proxy).setProtocolConfig(config);
+        vm.stopBroadcast();
 
         console.log("CommitProtocol proxy deployed to:", proxy);
         console.log("Protocol owner address set to:", owner);
         console.log("Protocol fee address set to:", protocolFeeRecipient);
-        vm.stopBroadcast();
     }
 }
