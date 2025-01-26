acd54500b761a5a28340db13e266cd76aa22af05
rac-sri <rac.sri25@gmail.com>
2025-01-26 19:50:16 +0530

    feat: add determinsitc deployment

--- src/Create2Factory.sol
+++ src/Create2Factory.sol
@@ -4,7 +4,10 @@ pragma solidity ^0.8.28;
 contract Create2Factory {
     error Create2FailedDeployment();
 
-    function deploy(bytes32 salt, bytes memory creationCode) external returns (address addr) {
+    function deploy(
+        bytes32 salt,
+        bytes memory creationCode
+    ) external returns (address addr) {
         assembly {
             addr := create2(
                 callvalue(),
@@ -13,18 +16,27 @@ contract Create2Factory {
                 salt
             )
         }
-        
+
         if (addr == address(0)) {
             revert Create2FailedDeployment();
         }
     }
 
-    function computeAddress(bytes32 salt, bytes memory creationCode) public view returns (address) {
-        return address(uint160(uint(keccak256(abi.encodePacked(
-            bytes1(0xff),
-            address(this),
-            salt,
-            keccak256(creationCode)
-        )))));
+    function computeAddress(
+        bytes32 salt,
+        bytes32 creationCodeHash
+    ) external view returns (address addr) {
+        address contractAddress = address(this);
+
+        assembly {
+            let ptr := mload(0x40)
+
+            mstore(add(ptr, 0x40), creationCodeHash)
+            mstore(add(ptr, 0x20), salt)
+            mstore(ptr, contractAddress)
+            let start := add(ptr, 0x0b)
+            mstore8(start, 0xff)
+            addr := keccak256(start, 85)
+        }
     }
 }
